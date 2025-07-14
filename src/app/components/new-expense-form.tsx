025d0425fb2db8b0aaf9162fd60f6c171438ebad
Jacob Boyce <jboyce313@gmail.com>
2025-07-14 17:18:39 -0500

    add submit button

--- src/app/components/new-expense-form.tsx
+++ src/app/components/new-expense-form.tsx
@@ -21,16 +21,22 @@ export default function NewExpenseForm() {
   };
 
   return (
-    <div className="flex flex-col items-center mb-4 bg-white p-4 rounded shadow">
-      <h2>Enter new expense:</h2>
+    <div className="flex flex-col mb-4 bg-white p-4 rounded shadow">
+      {/* <h2 className="mb-2 ml-0">Enter new expense:</h2> */}
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
           type="number"
-          placeholder="Enter amount"
+          placeholder="Enter new expense"
           value={amount}
           onChange={handleChange}
-          className="border border-gray-300 rounded"
+          className="border border-gray-300 rounded p-2 mb-2 w-full"
         />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white rounded p-2 w-full"
+        >
+          Add
+        </button>
       </form>
     </div>
   );
